b6a5473255854a19d3e9ba7ba1f64af3635ff126
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2025-12-14 22:32:46 +0100

    fix: rename about route to contact route

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    let d = new Date().toLocaleString("en-US", {
-        timeZone: "Europe/Warsaw",
-        hour12: false,
-        timeStyle: "short",
-    });
+  let d = new Date().toLocaleString('en-US', {
+    timeZone: 'Europe/Warsaw',
+    hour12: false,
+    timeStyle: 'short',
+  });
 </script>
 
 <div class="pt-6 flex gap-9 text-xl font-['Inter']">
-    <a href="/">__HOME</a>
-    <a href="/projects" class="pl-46">_PROJECTS(TODO)</a>
-    <a href="/about" class="pl-80">ABOUT</a>
-    <p class="ml-auto">LOCAL_TIME: {d}</p>
+  <a href="/">__HOME</a>
+  <a href="/projects" class="pl-46">_PROJECTS(TODO)</a>
+  <a href="/contact" class="pl-80">CONTACT</a>
+  <p class="ml-auto">LOCAL_TIME: {d}</p>
 </div>
