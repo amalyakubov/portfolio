70cbb107e658bdb7d62f49e15dafbb22988a29a6
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2025-12-14 22:52:00 +0100

    feat: improve typography of homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,15 @@
 </script>
 
 <div class="flex items-end justify-between w-full pt-20">
-  <p class="text-8xl pt-[40vh] pb-20 w-min">Amal Yakubov</p>
+  <p class="lg:text-9xl pt-[30vh] pb-20 w-min">Amal Yakubov</p>
   <div>
     <video autoplay muted loop playsinline width="900">
       <source src={video} type="video/mp4" />
     </video>
   </div>
 </div>
-<p class="text-xl pt-8 min-w-3xl w-min font-medium">
+<p class="text-3xl pt-8 min-w-3xl w-min font-medium leading-9.5">
   Hey, I’m Amal, a full stack developer and an aspiring ML researcher and a guy
-  who likes cool stuff and building really cool stuff. <br /> Based in the best city
-  of Europe: Warsaw, Poland.
+  who likes cool stuff and building really cool stuff. <br /> <br /> Based in the
+  best city of Europe: Warsaw, Poland.
 </p>
