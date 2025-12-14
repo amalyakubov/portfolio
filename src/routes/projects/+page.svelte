fae6d5e19b50e1f6c8474c8b530589c42cfc4cc2
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2025-12-14 21:14:08 +0100

    fix: remove redundnat import

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,7 @@
+<div class="cards-container"></div>
+
+<p class="comment">
+    And yet there is always room for more. No matter how far you’ve come, there
+    is always more to discover. This is just the beginning... Of many great
+    things yet to come.
+</p>
