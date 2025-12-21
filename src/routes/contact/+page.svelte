4211e2ccb0bc63e7b506bc94f87122b1ed79a2a3
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2025-12-21 18:00:53 +0100

    feat: improve graphics of contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,9 +1,9 @@
 <div class="flex flex-1 flex-col justify-between">
     <form
-        class="bg-[#ffff] px-12 py-10 flex flex-col max-w-3xl"
+        class="bg-[#ffff] px-12 py-10 flex flex-col max-w-3xl border border-black/20"
         id="contact_form"
     >
-        <div class="flex flex-row">
+        <div class="flex flex-row border-black/50 h-min pb-2">
             <label
                 for="email"
                 class="text-md min-w-max content-center pr-4 text-md w-30"
@@ -15,10 +15,10 @@
                 autocomplete="work email"
                 required
                 placeholder="your@gmail.com"
-                class="outline-none max-w-full px-2 py-4 placeholder:text-black/70 text-black/70"
+                class="outline-none max-w-full px-2 placeholder:text-black/70 text-black/70"
             />
         </div>
-        <div class="flex flex-row">
+        <div class="flex flex-row pt-2 h-mini border-black/50">
             <label
                 for="message"
                 class="text-md min-w-max content-center self-start pr-4 text-md w-30"
@@ -32,7 +32,7 @@
                 >... your message</textarea
             >
         </div>
-        <div>
+        <div class="pt-12">
             <button
                 form="contact_form"
                 title="Submit"
