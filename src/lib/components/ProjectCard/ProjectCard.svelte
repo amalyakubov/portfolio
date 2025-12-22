78feabb529bd5b34ad72608316d6d6f1583fd097
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2025-12-22 19:46:52 +0100

    feat: improve styling

--- src/lib/components/ProjectCard/ProjectCard.svelte
+++ src/lib/components/ProjectCard/ProjectCard.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div class="card">
-  <p class="pb-6 text-xl">{name}</p>
+  <p class="pb-8 text-3xl text-black/85 tracking-tighter">{name}</p>
   <a
     href="https://polhim.com/"
-    class="flex flex-1 overflow-hidden rounded-xs drop-shadow-2xl transition-all duration-300 hover:scale-105 ease-in-out h-100 w-100"
+    class="flex flex-1 overflow-hidden rounded-xs drop-shadow-2xl transition-all duration-300 hover:scale-105 ease-in-out h-80 w-80"
     aria-label={`View ${name} project`}
   >
     <img
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,12 +3,13 @@
   import polhimHero from "$lib/assets/polhim.png";
 </script>
 
-<div class="cardContainer pt-[30vh] pb-[10vh] flex flex-row gap-12">
+<p class="text-7xl pt-20">Things i've done</p>
+<div class="cardContainer pt-35 py-60 flex flex-row gap-12">
   <!-- <ProjectCard name="matura_prepper" imgSrc="null" alt="image" /> -->
   <ProjectCard name="polhim.com" imgSrc={polhimHero} alt="image" />
   <!-- <ProjectCard name="PostgresQL" imgSrc="null" alt="image" /> -->
 </div>
-<p class="comment text-2xl w-min min-w-200">
+<p class="comment text-xl w-min min-w-200">
   And yet there is always room for more. No matter how far you’ve come, there is
   always more to discover. This is just the beginning...<br /><br /> Of many great
   things yet to come.
