cc3e8d30a7b8052aa889c1139caed9532c069ed4
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2025-12-22 18:42:54 +0100

    feat: improve corners on showcase images

--- src/lib/components/ProjectCard/ProjectCard.svelte
+++ src/lib/components/ProjectCard/ProjectCard.svelte
@@ -6,7 +6,7 @@
   <p class="pb-6 text-xl">{name}</p>
   <a
     href="https://polhim.com/"
-    class="flex flex-1 overflow-hidden rounded-2xl drop-shadow-2xl transition-all duration-300 hover:scale-105 ease-in-out h-100 w-100"
+    class="flex flex-1 overflow-hidden rounded-xs drop-shadow-2xl transition-all duration-300 hover:scale-105 ease-in-out h-100 w-100"
     aria-label={`View ${name} project`}
   >
     <img
