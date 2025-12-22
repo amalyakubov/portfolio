bc635992982264b5385d520a9f15cf9c96ab0070
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2025-12-22 13:36:29 +0100

    feat: add a screenshot of polhim.com to showcase of projects

--- src/lib/components/ProjectCard/ProjectCard.svelte
+++ src/lib/components/ProjectCard/ProjectCard.svelte
@@ -4,5 +4,5 @@
 
 <div class="card md:min-w-96 md:min-h-96">
   <p class="pb-4">{name}</p>
-  <img src={imgSrc} {alt} />
+  <img width="600" src={imgSrc} {alt} />
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  import ProjectCard from '$lib/components/ProjectCard/ProjectCard.svelte';
+  import ProjectCard from "$lib/components/ProjectCard/ProjectCard.svelte";
+  import polhimHero from "$lib/assets/polhim.png";
 </script>
 
 <div class="cardContainer pt-[30vh] pb-[10vh] flex flex-row gap-4">
   <ProjectCard name="matura_prepper" imgSrc="null" alt="image" />
-  <ProjectCard name="polhim.com" imgSrc="null" alt="image" />
+  <ProjectCard name="polhim.com" imgSrc={polhimHero} alt="image" />
   <ProjectCard name="PostgresQL" imgSrc="null" alt="image" />
 </div>
 <p class="comment text-2xl w-min min-w-200">
