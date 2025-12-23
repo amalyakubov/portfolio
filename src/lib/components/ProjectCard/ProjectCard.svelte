86991e25226a38323ee390630d5e8f9fc2938f66
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2025-12-23 12:57:35 +0100

    feat: add white space between project description and project image

--- src/lib/components/ProjectCard/ProjectCard.svelte
+++ src/lib/components/ProjectCard/ProjectCard.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-    let { name, imgSrc, alt, text = null } = $props();
+  let { name, imgSrc, alt, text = null } = $props();
 </script>
 
 <div class="card w-min">
-    <p class="text-3xl text-black/85 tracking-tighter">{name}</p>
-    <a
-        href="https://polhim.com/"
-        class="flex flex-1 overflow-hidden rounded-xs drop-shadow-2xl transition-all duration-300 hover:scale-105 ease-in-out h-80 w-80 mt-12 mb"
-        aria-label={`View ${name} project`}
-    >
-        <img
-            src={imgSrc}
-            {alt}
-            class="object-cover w-full h-full hover:cursor-pointer"
-            loading="lazy"
-        />
-    </a>
-    <p class="w-full text-black/80">{text}</p>
+  <p class="text-3xl text-black/85 tracking-tighter">{name}</p>
+  <a
+    href="https://polhim.com/"
+    class="flex flex-1 overflow-hidden rounded-xs drop-shadow-2xl transition-all duration-300 hover:scale-105 ease-in-out h-80 w-80 mt-12 mb-9"
+    aria-label={`View ${name} project`}
+  >
+    <img
+      src={imgSrc}
+      {alt}
+      class="object-cover w-full h-full hover:cursor-pointer"
+      loading="lazy"
+    />
+  </a>
+  <p class="w-full text-black/80">{text}</p>
 </div>
